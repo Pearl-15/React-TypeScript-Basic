189f56d25b2baba89c34564f82a1c70f3cbb841d
Pearl Khin <pearlkhin@Pearls-Mac-mini.local>
2024-11-13 17:47:58 +0800

    feat: add header using props Obj type

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import CourseGoal from './components/CourseGoal.tsx';
+import Header from './components/Header.tsx';
+import goalsImg from './assets/goals.jpg'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +12,11 @@ function App() {
   return (
     <>
     <main>
+      <Header
+        image={{src: goalsImg, alt: 'A list of Goals'}}
+      >
+        <h1>Your Course Goals</h1>
+      </Header>
      <CourseGoal 
       title="Learn React"
      />
